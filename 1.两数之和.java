a2b153a064245cf89422a626881faa26d377252b
ImCaO <imcao@imcao.cn>
2022-03-09 23:58:20 +0800

    3.9

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * @lc app=leetcode.cn id=1 lang=java
  *
@@ -7,14 +10,14 @@
 // @lc code=start
 class TwoSum {
   public int[] twoSum(int[] nums, int target) {
-    for (int i = 0; i < nums.length - 1; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        if (nums[i] + nums[j] == target) {
-          return new int[] { i, j };
-        }
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      if (map.containsKey(target - nums[i])) {
+        return new int[] { i, map.get(target - nums[i]) };
       }
+      map.put(nums[i], i);
     }
-    return new int[] {};
+    return new int[] { 0, 0 };
   }
 }
 // @lc code=end

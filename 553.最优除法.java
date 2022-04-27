5c6269d6b718acddf5c14a155873b3dcd87fc223
ImCaO <imcao@imcao.cn>
2022-04-27 21:58:28 +0800

    4.27

---
+++ 553.最优除法.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=553 lang=java
+ *
+ * [553] 最优除法
+ */
+
+// @lc code=start
+class Solution {
+  public String optimalDivision(int[] nums) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < nums.length; i++) {
+      sb.append(nums[i]);
+      sb.append('/');
+    }
+    sb.deleteCharAt(sb.length() - 1);
+    if (nums.length > 2) {
+      sb.insert(sb.indexOf("/") + 1, '(');
+      sb.append(')');
+    }
+    return sb.toString();
+  }
+}
+// @lc code=end
